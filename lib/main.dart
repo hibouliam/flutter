03353b3688cb48f5d1fa8f75a19956f063d0d1d6
hibouliam <huissoudlam@aol.com>
2024-10-16 17:20:28 +0200

    m

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Button Example',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Exemple de Bouton Flutter'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              // Action qui sera exécutée lors du clic sur le bouton
-              print('Bouton cliqué!');
-            },
-            child: Text('Cliquez-moi'),
+        appBar: AppBar(title: Text('Carte Google Maps')),
+        body: GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: LatLng(48.8566, 2.3522), // Coordonnées de Paris
+            zoom: 12,
           ),
         ),
       ),
